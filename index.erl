abe00718230737d0e3c1a2e05c2db190ea962ceb
zax <zax@noemail.net>
2018-01-15 15:21:20 +0000

    Make a damn RSS feed
    FossilOrigin-Name: 347c3d20593268a0449a4467fd897acb49faa413

--- index.erl
+++ index.erl
@@ -1,18 +1,24 @@
 -module(index).
 -export([data/1, site/1]).
 
+-define(HOSTNAME, "blog.zdsmith.com").
+
 data(_) ->
     #{
        blog => {eterm, "subsetpark.config"},
        posts => {markdown, "posts/*.md"},
        pages => {markdown, "pages/*.md"},
        about => {markdown, "about.md"}
-    }.
+      }.
 
 site(Data) ->
     #{
        "site/index.html" =>
        {template, "templates/index.html", #{site_root => ""}},
+
+       "site/feed.xml" =>
+       {template, "templates/feeds.jinja",
+        #{site_root => "", host => ?HOSTNAME, context => get_context(Data)}},
 
        "site/posts/index.html" =>
        {template, "templates/posts.html", #{site_root => "../"}},
@@ -36,8 +42,8 @@ site(Data) ->
        "site/static/*.html" =>
        {files, "assets/*.html"},
 
-       "site/CNAME" => {string, "blog.zdsmith.com"}
-    }.
+       "site/CNAME" => {string, ?HOSTNAME}
+      }.
 
 is_post(Post) ->
     case lists:keyfind("status", 1, Post) of
@@ -45,6 +51,28 @@ is_post(Post) ->
             true;
         _ -> false
     end.
+
+datestr_to_822(DateStr) ->
+    {ok, DateTime} = tempo:parse(<<"%Y-%m-%d">>, list_to_binary(DateStr), datetime),
+    case DateTime of
+        format_mismatch ->
+            io:format("Can't convert date: ~p~n", [DateStr]),
+            erlang:error(date_format_mismatch);
+        _ -> ok
+    end,
+    {ok, Formatted} = tempo:format(rfc2822, DateTime, datetime),
+    Formatted.
+
+add_post_context(Post) ->
+  {_, Date} = lists:keyfind("date", 1, Post),
+  Rfc822 = datestr_to_822(Date),
+  lists:keystore("rss_date", 1, Post, {"rss_date", Rfc822}).
+
+get_context(Data) ->
+    code:add_path("src/tempo/ebin"),
+    Posts = posts(Data),
+    PostContext = lists:map(fun add_post_context/1, Posts),
+    PostContext.
 
 posts(Data) ->
     Posts = plist:value(posts, Data),
