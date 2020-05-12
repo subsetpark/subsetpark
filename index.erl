1c9b1030559fc2f0800eae0eab31c861a1d9970b
zax <zax@noemail.net>
2020-05-12 02:25:53 +0000

    Decorate main note with related notes
    FossilOrigin-Name: a9db83a84bcb3d2f5fd61b3ebc6cd66723aa2f7d

--- index.erl
+++ index.erl
@@ -1,8 +1,9 @@
 -module(index).
 
--export([data/1, related_notes/1, site/1]).
+-export([data/1, related_notes/1, site/1, with_hyperlinks/2]).
 
 -define(HOSTNAME, "blog.zdsmith.com").
+-define(ROOT, "../").
 
 %
 % API
@@ -18,31 +19,26 @@ data(_) ->
 site(Data) ->
     Notes = notes(Data),
     Notes2 = with_metadata(Notes),
-    #{"site/index.html" =>
-          {template, "templates/index.html", #{site_root => ""}},
+    #{"site/index.html" => {template, "templates/index.html", #{site_root => ""}},
       "site/feed.xml" =>
-          {template, "templates/feeds.jinja",
-           #{site_root => "", host => ?HOSTNAME,
-             context => get_context(Data)}},
-      "site/posts/index.html" =>
-          {template, "templates/posts.html",
-           #{site_root => "../"}},
+          {template,
+           "templates/feeds.jinja",
+           #{site_root => "", host => ?HOSTNAME, context => get_context(Data)}},
+      "site/posts/index.html" => {template, "templates/posts.html", #{site_root => ?ROOT}},
       "site/notes/index.html" =>
-          {template, "templates/notes.html",
-           #{site_root => "../", notes => Notes2}},
+          {template, "templates/notes.html", #{site_root => ?ROOT, notes => Notes2}},
       "site/pages/{{page.title|slugify}}.html" =>
-          {template_map, "templates/page.html",
-           {page, pages(Data)}, #{site_root => "../"}},
+          {template_map, "templates/page.html", {page, pages(Data)}, #{site_root => ?ROOT}},
       "site/posts/{{post.title|slugify}}.html" =>
-          {template_map, "templates/post.html",
-           {post, posts(Data)}, #{site_root => "../"}},
+          {template_map, "templates/post.html", {post, posts(Data)}, #{site_root => ?ROOT}},
       "site/notes/{{note.topic|slugify}}.html" =>
-          {template_map, "templates/note.html", {note, Notes2},
-           #{site_root => "../", all_notes => Notes2}},
+          {template_map,
+           "templates/note.html",
+           {note, Notes2},
+           #{site_root => ?ROOT, all_notes => Notes2}},
       "site/assets/*.css" => {files, "assets/*.css"},
       "site/images/*.png" => {files, "images/*.png"},
-      "site/images/current/*.png" =>
-          {files, "images/current/*.png"},
+      "site/images/current/*.png" => {files, "images/current/*.png"},
       "site/images/*.gif" => {files, "images/*.gif"},
       "site/static/*.html" => {files, "assets/*.html"},
       "site/CNAME" => {string, ?HOSTNAME}}.
@@ -53,28 +49,28 @@ site(Data) ->
 
 is_post(Post) ->
     case lists:keyfind("status", 1, Post) of
-      {"status", "post"} -> true;
-      _ -> false
+      {"status", "post"} ->
+          true;
+      _ ->
+          false
     end.
 
 datestr_to_822(DateStr) ->
-    {ok, DateTime} = tempo:parse(<<"%Y-%m-%d">>,
-                                 list_to_binary(DateStr), datetime),
+    {ok, DateTime} = tempo:parse(<<"%Y-%m-%d">>, list_to_binary(DateStr), datetime),
     case DateTime of
       format_mismatch ->
           io:format("Can't convert date: ~p~n", [DateStr]),
           erlang:error(date_format_mismatch);
-      _ -> ok
+      _ ->
+          ok
     end,
-    {ok, Formatted} = tempo:format(rfc2822, DateTime,
-                                   datetime),
+    {ok, Formatted} = tempo:format(rfc2822, DateTime, datetime),
     Formatted.
 
 add_post_context(Post) ->
     {_, Date} = lists:keyfind("date", 1, Post),
     Rfc822 = datestr_to_822(Date),
-    lists:keystore("rss_date", 1, Post,
-                   {"rss_date", Rfc822}).
+    lists:keystore("rss_date", 1, Post, {"rss_date", Rfc822}).
 
 get_context(Data) ->
     code:add_path("src/tempo/ebin"),
@@ -85,9 +81,11 @@ posts(Data) ->
     Posts = plist:value(posts, Data),
     [V1 || V1 <- Posts, is_post(V1)].
 
-pages(Data) -> plist:value(pages, Data).
+pages(Data) ->
+    plist:value(pages, Data).
 
-notes(Data) -> plist:value(notes, Data).
+notes(Data) ->
+    plist:value(notes, Data).
 
 with_metadata(Notes) ->
     [with_topic(with_contents(V1)) || V1 <- Notes].
@@ -111,19 +109,38 @@ note_content(NoteMeta) ->
 note_filename(NoteMeta) ->
     plist:value('__file__', NoteMeta).
 
+note_path(NoteMeta) ->
+    [<<?ROOT>>, <<"notes/">>, erlydtl_filters:slugify(note_topic(NoteMeta)), <<".html">>].
+
 %
 % Custom Filters
 %
 
+with_hyperlinks(MainNote, AllNotes) ->
+    lists:foldl(fun (NoteMeta, Acc) ->
+                        RelatedTopic = note_topic(NoteMeta),
+                        CP = binary:compile_pattern(list_to_binary(RelatedTopic)),
+                        case is_related(MainNote, CP, RelatedTopic) of
+                          true ->
+                              link_cp(Acc, CP, NoteMeta);
+                          _ ->
+                              Acc
+                        end
+                end,
+                note_content(MainNote),
+                AllNotes).
+
 related_notes([Topic, AllNotes]) ->
     CP = binary:compile_pattern(list_to_binary(Topic)),
-    Related = [V1
-               || V1 <- AllNotes, is_related(V1, CP, Topic)],
-    [[<<"<h2>">>, note_topic(NoteMeta), <<"</h2>">>,
-      <<"<p>">>,
-      lpad_markdown:to_html(highlight_cp(note_content(NoteMeta),
-                                         CP)),
-      <<"</p>">>]
+    Related = [V1 || V1 <- AllNotes, is_related(V1, CP, Topic)],
+    [[<<"<h2>">>,
+      <<"<a href=">>,
+      note_path(NoteMeta),
+      <<">">>,
+      note_topic(NoteMeta),
+      <<"</a>">>,
+      <<"</h2>">>,
+      lpad_markdown:to_html(highlight_cp(note_content(NoteMeta), CP))]
      || NoteMeta <- Related].
 
 %
@@ -132,14 +149,20 @@ related_notes([Topic, AllNotes]) ->
 
 is_related(NoteMeta, CP, Topic) ->
     NoteContent = note_content(NoteMeta),
-    case {note_topic(NoteMeta),
-          binary:match(NoteContent, CP)}
-        of
-      {Topic, _} -> false;
-      {_, nomatch} -> false;
-      _ -> true
+    case {note_topic(NoteMeta), binary:match(NoteContent, CP)} of
+      {Topic, _} ->
+          false;
+      {_, nomatch} ->
+          false;
+      _ ->
+          true
     end.
 
 highlight_cp(NoteContent, CP) ->
-    binary:replace(NoteContent, CP, <<"**">>,
-                   [global, {insert_replaced, 1}]).
+    binary:replace(NoteContent, CP, <<"**">>, [global, {insert_replaced, 1}]).
+
+link_cp(NoteContent, CP, RelatedMeta) ->
+    [binary:replace(NoteContent, CP, <<"[]">>, [global, {insert_replaced, 1}]),
+     <<"(">>,
+     note_path(RelatedMeta),
+     <<")">>].
